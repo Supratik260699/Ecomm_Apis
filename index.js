4b479313c49e5a096bd2cd14c9509c8f086f0194
Supratik260699 <62494112+Supratik260699@users.noreply.github.com>
2022-04-19 15:00:52 +0530

    First commit

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect("mongodb+srv://admin:broadband@cluster0.z5jxk.mongodb.net/eComDB",{ useNewUrlParser: true});   //https://cloud.mongodb.com/v2/625d24881c48766ffe5fa246#metrics/replicaSet/625d27100cbb8a7c6b6d1122/explorer/eComDB/ecoms/find
 app.use('/',require('./routes'));
-app.listen("1200",function(req,res){
+app.listen("process.env.PORT || 1200",function(req,res){
     console.log("Server is running on port 1200");
 });
