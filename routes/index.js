063333300e9de2b0a005cfd74c32a4a445b4cd82
Supratik260699 <62494112+Supratik260699@users.noreply.github.com>
2022-05-03 21:46:46 +0530

    3rd Commit

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 //Router used
 router.use('/products',require('./products'));
-
+router.get('/',function(req,res){
+    return res.end('Add /products in postman to check the Api');
+})
 module.exports = router //to export 
